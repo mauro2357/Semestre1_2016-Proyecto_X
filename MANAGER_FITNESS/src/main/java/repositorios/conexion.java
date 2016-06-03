040108c39209436de327ba7d9be68fd1ecade835
kmiloramirez <kamiloperlaza-1996@hotmail.com>
2016-06-02 22:30:54 -0500

    conexion
    con

--- MANAGER_FITNESS/src/main/java/repositorios/conexion.java
+++ MANAGER_FITNESS/src/main/java/repositorios/conexion.java
@@ -10,7 +10,6 @@ public class conexion {
 		    if(con!=null) return con;
 			String driver = "com.mysql.jdbc.Driver";
 		    Class.forName(driver);
-		    con = DriverManager.getConnection("jdbc:mysql://mysql12198-managerfitness.j.facilcloud.com:11005/bd_manager_fitness","root", "Cb1LkP3zeL");
 		    //con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_manager_fitness","root", "root");
 		    return con;
 		
